2552678af6d49f2119d78ebc15620e57e4026ce3
Milton Camelo <mcamelo@picap.co>
2020-09-25 22:52:17 -0500

    Adding validations with useRef

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,14 +1,53 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 const Login = () => {
 
+  const mailRef = useRef();
+  const passwordRef = useRef();
+
+  const [mailError, setMailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+  const [formErrors, setFormErrors] = useState([]);
   const [showSecret, setShowSecret] = useState(false)
 
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    console.log('Submitted!')
+    const mail = mailRef.current;
+    const password = passwordRef.current;
+
+    const errors = handleValidation(mail, password)
+    setFormErrors(errors)
+
+    if (errors.length === 0) {
+      console.log('REDUX/REMATCH LOGIC')
+      setMailError(false)
+      setPasswordError(false)
+
+    }
+  }
+
+  const handleValidation = (mail, password) => {
+
+    const errors = [];
+
+    if (mail.value.length === 0) {
+      errors.push('E-mail cannot be empty');
+      setMailError(true);
+    }
+
+    if (!/[A-Za-z0-9]{1,}\@[A-Za-z0-9]{1,}\.[A-Za-z]{1,}/.test(mail.value)) {
+      errors.push('Invalid email format');
+      setMailError(true);
+    }
+
+    if (password.value.length === 0) {
+      errors.push('Password cannot be empty');
+      setPasswordError(true);
+    }
+
+    return errors
   }
 
   return (
@@ -17,22 +56,32 @@ const Login = () => {
       <div className="container h-100">
         <div className="row h-100 form">
           <div className="col col-lg-5">
+
+            {
+              formErrors.map((error, index) => {
+                return <div key={index} className="alert alert-danger info">
+                  {error}
+                </div>
+              })
+            }
+
             <form className="px-3 m-0" onSubmit={handleSubmit} noValidate>
               <div className="form-group">
                 <input
                   type="email"
-                  className="form-control input"
+                  className={`form-control input ${mailError ? 'is-invalid' : 'is-valid'}`}
                   placeholder="Your E-mail"
                   autoComplete="false"
+                  ref={mailRef}
                   required />
-
               </div>
 
               <div className="form-group position-relative spacer">
                 <input
                   type={showSecret ? 'text' : 'password'}
                   placeholder="Your password"
-                  className="form-control input"
+                  className={`form-control input ${passwordError ? 'is-invalid' : 'is-valid'}`}
+                  ref={passwordRef}
                   required />
                 <div
                   className="font-weight-bold rounded-pill bg-white px-2 position-absolute password-btn"
